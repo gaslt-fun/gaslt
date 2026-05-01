e2fbbde8d46a5b39e13e37378b600dd5890725b2
gaslt-fun <216176888+gaslt-fun@users.noreply.github.com>
2026-05-01 09:09:09 +0900

    configure jest with ts-jest in esm mode

---
+++ sdk/jest.config.js
@@ -0,0 +1,23 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        useESM: true,
+        tsconfig: {
+          module: "ESNext",
+          moduleResolution: "Bundler",
+          verbatimModuleSyntax: false,
+        },
+      },
+    ],
+  },
+  testMatch: ["**/tests/**/*.test.ts"],
+};
